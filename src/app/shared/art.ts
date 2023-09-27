0269879b127fe8cd440a85fff0600468b8783c65
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-09-27 10:12:58 -0400

    updated art class and added sample data to test

--- src/app/shared/art.ts
+++ src/app/shared/art.ts
@@ -1,18 +1,79 @@
+import { create } from "domain"
 import { Entity, Fields } from "remult"
 
 @Entity("art", {
   allowApiCrud: true
 })
-export class Art {
-  @Fields.cuid()
-  id = ""
+export class ArtPiece {
+  @Fields.integer()
+  catalogNum = ""
 
   @Fields.string()
   title = ""
 
-  @Fields.boolean()
-  completed = false
+  @Fields.string()
+  artist = ""
 
   @Fields.createdAt()
-  createdAt?: Date
+  created?: Date
+  
+  @Fields.string()
+  description = ""
+
+  @Fields.string()
+  imageUrl = ""
+
+  @Fields.boolean()
+  isSold = false
+
+  @Fields.string()
+  medium = ""
+
+  @Fields.string()
+  size = ""
+
+  @Fields.string()
+  location = ""
+
+
 }
+
+// sample data
+let ArtData = [
+  {
+    catalogNum: 1,
+    title: "The Scream",
+    artist: "Edvard Munch",
+    created: new Date("1893-01-01"),
+    description: "The Scream is the popular name given to a composition created by Norwegian Expressionist artist Edvard Munch in 1893.",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/The_Scream.jpg/800px-The_Scream.jpg",
+    isSold: false,
+    medium: "Oil, tempera, pastel and crayon on cardboard",
+    size: "91 cm × 73.5 cm (36 in × 28.9 in)",
+    location: "National Gallery, Oslo, Norway"
+  },
+  {
+    catalogNum: 2,
+    title: "Mona Lisa",
+    artist: "Leonardo da Vinci",
+    created: new Date("1503-01-01"),
+    description: "The Mona Lisa is a half-length portrait painting by Italian artist Leonardo da Vinci. It is considered an archetypal masterpiece of the Italian Renaissance.",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/Mona_Lisa.jpg/800px-Mona_Lisa.jpg",
+    isSold: false,
+    medium: "Oil on poplar panel",
+    size: "77 cm × 53 cm (30 in × 21 in)",
+    location: "Musée du Louvre, Paris, France"
+  },
+  {
+    catalogNum: 3,
+    title: "The Starry Night",
+    artist: "Vincent van Gogh",
+    created: new Date("1889-01-01"),
+    description: "The Starry Night is an oil on canvas by the Dutch post-impressionist painter Vincent van Gogh. Painted in June 1889, it depicts the view from the east-facing window of his asylum room at Saint-Rémy-de-Provence.",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg/800px-Van_Gogh_-_Starry_Night_-_Google_Art_Project.jpg",
+    isSold: false,
+    medium: "Oil on canvas",
+    size: "73.7 cm × 92.1 cm (28.7 in × 36.3 in)",
+    location: "Museum of Modern Art, New York City, United States"
+  }
+]
