f07484edd9901debed19a6131defc9317baf335a
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-10-25 13:35:30 -0400

    added role enum

--- src/app/shared/user.ts
+++ src/app/shared/user.ts
@@ -13,6 +13,14 @@ export class User {
     @Fields.string()
     password = ''
 
-    @Fields.string()
-    role: 'admin' | 'user' = 'admin' // default is admin for V1, since those will be the only users
+    @Fields.object()
+    roles = [Role.Admin] // default is admin for V1, since those will be the only users
+    // we assign it as Role.Admin, but it is stored as 0, to make the roles anonymous. It also saves space in the database.
+}
+
+export enum Role {
+  Admin, // = 0
+  Student, // = 1
+  Teacher,
+  Volunteer
 }
