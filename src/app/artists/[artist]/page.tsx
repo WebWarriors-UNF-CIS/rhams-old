04d7bbbd539501863d3ce8ba89163d6ae042e8c7
amberredmond <n01456296@unf.edu>
2023-11-09 04:05:50 -0500

    Cleaned up divs- fixed table
    also added buttons to view art from the artist page which takes you to the art page for right now. Re-positioned containers in the manage artist page with grid instead of flex

--- src/app/artists/[artist]/page.tsx
+++ src/app/artists/[artist]/page.tsx
@@ -16,30 +16,40 @@ async function ArtistView({ params }: { params: { artist: string } }) {
   return (
     <div className="mt-8 grid grid-cols-2 gap-5">
         <div>
-            <h1 className="dark:text-white text-2xl"  >artist name</h1>
+            <h1 className="dark:text-white text-2xl"  >Reuben Hale</h1>
             <Image className="max-w-xs"src={reubenPic} alt="Reuben Hale" />
         </div>
         <div>
             <div>
-                <h1 className="dark:text-white text-2xl">{params.artist}</h1>
+                <h1 className="dark:text-white text-2xl">Artworks</h1>
                 <table>
                     <thead>
                     <tr className="border border-solid ">
-                        <th className="">Medium</th>
-                        <th>Title</th>
-                        <th className="">Date</th>
+                        <th className="border border-solid p-4">Title</th>
+                        <th className="border border-solid p-4">Date</th>
+                        <th className="border border-solid p-4">Medium</th>
+                        <td className="border border-solid p-4">
+                        </td>
                     </tr>
                     </thead>
                     <tbody>
                     <tr>
-                        <td>Oil on Canvas</td>
-                        <td>The Starry Night</td>
-                        <td>1889</td>
+                        <td className="border border-solid p-4">Sculpture of a Woman</td>
+                        <td className="border border-solid p-4">1973</td>
+                        <td className="border border-solid p-4">Clay Sculpture</td>
+                        <td className='border border-solid p-4 px-4 py-2'><a href="../art">
+                            <button  className="bg-blue-300 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded">View</button>
+                            </a>
+                        </td>
                     </tr>
                     <tr>
-                        <td>Watercolor</td>
-                        <td>Sunflowers</td>
-                        <td>1888</td>
+                        <td className="border border-solid p-4">Painting of a Prince</td>
+                        <td className="border border-solid p-4">1984</td>
+                        <td className="border border-solid p-4">Oil on Canvas</td>
+                        <td className='border border-solid p-4 px-4 py-2'><a href="../art">
+                            <button  className="bg-blue-300 hover:bg-blue-700 text-white font-bold py-1 px-1 rounded">View</button>
+                            </a>
+                        </td>
                     </tr>
         
                     </tbody>
