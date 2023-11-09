b72cd804f070bd0f95104b77a846140ae53d76cf
Harrison <hmichas@gmail.com>
2023-11-09 02:37:24 -0500

    Added card responsiveness, fixed layout

--- src/app/components/ExhibitCard.tsx
+++ src/app/components/ExhibitCard.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import { Exhibit } from '../shared/exhibit';
+import { useRouter } from 'next/navigation';
 
 interface ExhibitCardProps {
   exhibit: Exhibit;
 }
 
 const ExhibitCard: React.FC<ExhibitCardProps> = ({ exhibit }) => {
+    const router = useRouter();
     return (
-        <div className="border rounded-md border-gray-300 p-4 cursor-pointer">
+        <div className="border rounded-md border-gray-300 hover:border-gray-500 p-4 h-60 w-64 cursor-pointer"
+             onClick={() => router.push('exhibitions/view')}>
             <h3>{exhibit.name}</h3>
-            <p>{exhibit.location}</p>
-            <p>Dates: {exhibit.startDate.toString()} - {exhibit.endDate.toString()}</p>
+            <p className='italic'>{exhibit.location}</p>
+            <p>Start: {exhibit.startDate.toDateString()}</p>
+            <p>End: {exhibit.endDate.toDateString()}</p>
             {/* Could add more exhibit details here */}
         </div>
     );
