e1c42087c4e414cc7526bab2164205171f51dfa4
Harrison <hmichas@gmail.com>
2023-12-08 17:50:04 -0500

    navbar format

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -5,31 +5,35 @@ import logo from 'src/images/temp-logo.png'
 
 export default function Nav() {
     return (
-        <nav className="flex w-screen h-fit place-content-between items-center p-4 bg-emerald-400/90 text-white font-bold">
-            <a className="left-5" href="/"><Image className="px-4" width={150} height={150} src={logo} alt="Temporary Logo"/></a>
-            <div className=''>
-                <Link className="px-5 py-15 basis-1/7" href="/art">
+        <nav className="px-4 lg:px-6 bg-emerald-400/90 text-white font-bold">
+            <div className='flex flex-nowrap justify-between items-center mx-auto min-w-fit py-2'>
+                <a className="flex-shrink-0" href="/">
+                    <Image width={100} height={100} src={logo} alt="Temporary Logo"/>
+                </a>
+                <div className='flex flex-nowrap'>
+                <Link className="px-10 basis-1/6 hover:text-slate-100" href="/art">
                     <span>Art</span>
                 </Link>
-                <Link className="px-5 basis-1/7" href="/artists/manage">
+                <Link className="px-10 basis-1/6 hover:text-slate-100" href="/artists/manage">
                     <span>Artists</span>
                 </Link>
-                <Link className="px-5 basis-1/7" href="/exhibitions">
+                <Link className="px-10 basis-1/6 hover:text-slate-100" href="/exhibitions">
                     <span>Exhibitions</span>
                 </Link>
-                <Link className="px-5 basis-1/7" href="/literature">
+                <Link className="px-10 basis-1/6 hover:text-slate-100" href="/literature">
                     <span>Literature</span>
                 </Link>
-                <Link className="px-5 basis-1/7" href="/media">
+                <Link className="px-10 basis-1/6 hover:text-slate-100" href="/media">
                     <span>Media</span>
                 </Link>
-                <Link className="px-5 basis-1/7" href="/sales">
+                <Link className="px-10 basis-1/6 hover:text-slate-100" href="/sales">
                     <span>Sales</span>
                 </Link>
-                <Link className="px-5 basis-1/7" href="/login">
-                    
-                </Link>
+                </div>
+                <a className="flex-shrink-0" href="/login">
+                    <Image width={30} height={30} src={userico} alt="User Icon White"/>
+                </a>
             </div>
         </nav>
     );
-}
+} 
