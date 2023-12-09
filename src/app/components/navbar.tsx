c0e2b1a33340eff01b223b5e84b761fe5a93afe8
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-12-09 18:48:56 -0500

    making navbar responsive -incomplete

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,33 +1,36 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import userico from 'src/images/user-icon-lg-w.png'
+import userico from 'src/images/user-icon-lg-b.png'
 import logo from 'src/images/temp-logo.png'
+
+function NavLink({ href, inner }: { href: string, inner: string | JSX.Element}) {
+    return(
+        <li>
+            <Link href={href} className='px-5'>
+                <span>{inner}</span>
+            </Link>
+        </li>
+    )
+}
 
 export default function Nav() {
     return (
-        <nav className="flex w-screen justify-end items-center p-4 bg-emerald-400/90 text-white font-bold">
-            <a className="text-2xl font-bold absolute left-5" href="/"><Image className="px-4" width={150} height={150} src={logo} alt="Temporary Logo"/></a>
-            <Link className="px-5" href="/art">
-                <span>Art</span>
+        <nav className= "sticky top-0 w-screen px-4 bg-gradient-to-r from-emerald-800 to-[310px] to-emerald-500 text-black ">
+            <Link className="text-2xl font-bold absolute left-5" href="/">
+                <Image className="p-1" width={150} height={150} src={logo} alt="Temporary Logo"/>
             </Link>
-            <Link className="px-5" href="/artists/manage">
-                <span>Artists</span>
-            </Link>
-            <Link className="px-5" href="/exhibitions">
-                <span>Exhibitions</span>
-            </Link>
-            <Link className="px-5" href="/literature">
-                <span>Literature</span>
-            </Link>
-            <Link className="px-5" href="/media">
-                <span>Media</span>
-            </Link>
-            <Link className="px-5" href="/sales">
-                <span>Sales</span>
-            </Link>
-            <Link className="px-5" href="/login">
-                <span><Image width={30} height={30} src={userico} alt="User Icon White"/></span>
-            </Link>
+            <ul id="navigation" className="flex justify-end items-center max-sm:absolute max-sm:inset">
+                <NavLink href="/art" inner="Art"/>
+                <NavLink href="/artists/manage" inner="Artists"/>
+                <NavLink href="/exhibitions" inner="Exhibitions"/>
+                <NavLink href="/literature" inner="Literature"/>
+                <NavLink href="/media" inner="Media"/>
+                <NavLink href="/sales" inner="Sales"/>
+                <NavLink href="/login" inner={<Image width={30} height={30} src={userico} alt="User Icon White"/>}/>
+            </ul>
+            {/*<button id="nav-toggle" aria-controls="navigation" aria-expanded="false" className='!shadow-none'>
+                <span className="sr-only">Menu</span>
+            </button>*/}
         </nav>
     );
 }
