3954844829af6d90f7a0b0831d22518c214d0b5d
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-12-11 15:23:08 -0500

    updated config for images from wikipedia for demo

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { // used for viewing demo images, delete if the client will not be using images from Wikimedia Commons
+        protocol: 'https',
+        hostname: 'upload.wikimedia.org',
+        port: '',
+      },
+    ],
+  },
+}
 
 module.exports = nextConfig
