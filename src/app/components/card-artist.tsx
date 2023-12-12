cffdc325d6406dc984f720813d1adb8f3503225a
amberredmond <n01456296@unf.edu>
2023-12-12 00:45:14 -0500

    green button stuff

--- src/app/components/card-artist.tsx
+++ src/app/components/card-artist.tsx
@@ -21,6 +21,9 @@ const ArtistCard: React.FC<ArtistCardProps> = ({ artist }) =>
         <div className="card" onClick={() => router.push('./view')}>
             <h3>{artist.firstName}</h3>
             <h3>{artist.lastName}</h3>
+            <button className="btn-green mx-4 w-20">
+                Edit
+            </button>
         </div>
     );
 };
