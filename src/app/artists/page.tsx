46cde76d572e47b9a39be1f56ab9e898e8ea9023
amberredmond <n01456296@unf.edu>
2023-12-12 05:32:38 -0500

    added artist-card to globalcss, styled card-artist

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -26,16 +26,47 @@ export default function ArtistPage() {
       <Head>
         <title>View Artists</title>
       </Head>
-      <div className='flex flex-col mx-auto mt-10 pl-28'><h1>Artists</h1></div>
-      <div className="p-4 flex items-end justify-end">
-          <div className="space-x-4 grid grid-cols-2">
+      
+      <div>
+        <h1 className="text-center justify-text-3xl font-bold p-12 dark:text-white">Artists</h1>
+      <div className="grid grid-cols-7">
+        <div className='col-span-1'>
+          <h1 className="ml-10 dark:text-white text-xl  font-semibold whitespace-nowrap pr-1 pt-2">Find an Artist</h1>
+        </div>
+        <div className="mt-3">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="px-1  border rounded-lg focus:outline-none focus:ring focus:border-emerald-500"
+          />
           </div>
+        <div className="mt-3">
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="px-1 ml-3 border rounded-lg focus:outline-none focus:ring focus:border-emerald-500"
+          />
+        </div>
+        <div className="mt-3">
+          <button className="ml-3 bg-emerald-500 text-white rounded-md p-1 hover:bg-emerald-600 hover:shadow">
+            Search
+          </button>
+        </div>
+        <div className="col-span-3">
+          <a href="../artists/create"><button className="ml-10 bg-emerald-500 text-white rounded-md  mt-3 p-1 hover:bg-emerald-600 hover:shadow">
+              Add Artist
+          </button></a>
+        </div>
       </div>
+      </div>
+
+
+      
 
       <div className='flex flex-col justify-around items-center mx-auto mt-10'>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 mx-auto">
             {artists.map((artist) => (
-            <ArtistCard key={artist.id} artist={artist}/>
+            <ArtistCard key={artist.id} artist={artist} revalidate={1}/>
             ))}
         </div>
       </div>
