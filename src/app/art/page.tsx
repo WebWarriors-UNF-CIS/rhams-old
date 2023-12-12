efca0eca234ac145fd0b66234f9c9a9c2c278842
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-12-12 16:51:05 -0500

    added filters section (not complete)

--- src/app/art/page.tsx
+++ src/app/art/page.tsx
@@ -4,31 +4,83 @@ import { remult } from "remult"
 import { ArtPiece } from "../shared/art"
 import Link from "next/link"
 import ArtCard from "../components/artCard"
+import { Type } from "../shared/artist"
 
 const artRepo = remult.repo(ArtPiece)
 
 export default function ManageArtists() {
   const [Art, setArts] = useState<ArtPiece[]>([])
 
+  function toggleFilters() {
+    let filters = document.getElementById("filters");
+    if (filters!.style.display === "none") {
+      filters!.style.display = "flex";
+    } else {
+      filters!.style.display = "none";
+    }
+  }
+  
+
   useEffect(() => {
     artRepo.find().then(setArts)
   }, [])
   return (
-    <main className='dark:text-white'>
-      <Link href="/art/create" className="fixed btn-gray h-fit self-end right-5 top-24 text-white text-center font-semibold px-4 py-2 rounded-lg shadow-slate-900 shadow-[0_1px_2px_1px_rgba(0,0,0,0.25)]">New Art</Link>
-      <div className="float-left ml-6 flex flex-col w-40 border-2 border-black rounded-xl">
-        <div className="text-center p-4"> Painting </div>
-        <div className="text-center p-4 border-y border-t-2 border-black"> Sculpture </div>
-        <div className="text-center p-4 border-y border-black"> Photography </div>
-        <div className="text-center p-4 border-y border-black"> Drawing </div>
-        <div className="text-center p-4 border-y border-black"> Print </div>
-        <div className="text-center p-4 border-y border-black"> Furniture </div>
-        <div className="text-center p-4 border-y border-black border-b-2"> Hologram </div>
-        <div className="text-center p-4"> Other </div>
+    <main className='dark:text-white mt-2'>
+      <Link href="/art/create" className="fixed btn-green h-fit self-end right-5 top-24 text-white text-center font-semibold px-4 py-2 rounded-lg shadow-slate-900 shadow-[0_1px_2px_1px_rgba(0,0,0,0.25)]">New Art</Link>
+      <div className="float-left ml-6 mt-12 border-2 border-black rounded-xl">
+        {Object.values(Type).filter(value => isNaN(Number(value))).map((type) => (
+          <div className="text-center p-4 border-y border-black first:border-0 first:border-b last:border-0 last:border-t" key={type}> {type} </div>
+        ))}
       </div>
       <div>
-        <div className="m-3 p-1"> filters </div>
-        <div className="flex mx-12">
+        <button className="p-1 px-2 btn-gray" onClick={toggleFilters}> Filters </button>
+        <div className="relative flex-wrap w-3/4 m-2 left-4 hidden border border-black rounded-2xl" id="filters">
+          <div>
+            <label className="m-3 p-1"> Catalog # </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Title </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Artist </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Aquired </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Created </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Medium </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Height </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Width </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Depth </label>
+            <input/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Location </label>
+            <input className="m-3 p-1"/>
+          </div>
+          <div>
+            <label className="m-3 p-1"> Exhibit </label>
+            <input className="m-3 p-1"/>
+          </div>
+        </div>
+        <div className="flex mx-12 flex-wrap">
           {Art.map(art => {
             return (
               <div key={art.id}>
