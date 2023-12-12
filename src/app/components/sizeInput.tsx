043ed87f1f28737af1517f6dba0f900dba8767cd
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2023-12-12 16:55:34 -0500

    made dynamic

--- src/app/components/sizeInput.tsx
+++ src/app/components/sizeInput.tsx
@@ -1,18 +1,20 @@
-export default function SizeInput() {
+export default function SizeInput({ id }: { id: string}) {
   return (
   <div className='input grow'>
-    <label htmlFor="height">Height:</label>
-    <div className='bg-white group border border-black text-sm rounded-lg content flex focus:ring-black focus:border-emerald-500'>
+    <label htmlFor={id.toLowerCase()}>{id}:</label>
+    <div className='bg-white group border border-black text-sm rounded content flex focus:ring-black focus:border-emerald-500'>
       <input
         type="text"
-        id="height"
+        id={id.toLowerCase()}
         placeholder='20'
-        className="!inline !w-20 grow !border-none !outline-none rounded-xl"
+        className="!inline !w-20 grow !border-none !outline-none rounded"
       />
-      <div className="grow text-sm rounded-xl focus:outline-none
+      <div className="grow text-sm rounded focus:outline-none
       dark:bg-gray-700 dark:border-gray-600 dark:text-white">
-        <option value="inches">inches</option>
-        <option value="cm">cm</option>
+        <datalist>
+          <option value="inches">inches</option>
+          <option value="cm">cm</option>
+        </datalist>
       </div>
     </div>
   </div>
