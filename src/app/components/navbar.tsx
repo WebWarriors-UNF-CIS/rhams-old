ce800009a577a8ff4b1bd0a5781fc36864e128ae
Harrison <hmichas@gmail.com>
2023-12-11 22:55:41 -0500

    changed text color and reworked structure to justify between

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import userico from 'src/images/user-icon-lg-b.png'
+import userico from 'src/images/user-icon-lg-w.png'
 import logo from 'src/images/temp-logo.png'
 
 function NavLink({ href, inner }: { href: string, inner: string | JSX.Element}) {
@@ -15,19 +15,21 @@ function NavLink({ href, inner }: { href: string, inner: string | JSX.Element}) 
 
 export default function Nav() {
     return (
-        <nav className= "sticky top-0 w-screen px-4 bg-gradient-to-r from-emerald-800 to-[310px] to-emerald-400 text-black ">
-            <Link className="text-2xl font-bold absolute left-5" href="/">
-                <Image className="p-1" width={150} height={150} src={logo} alt="Temporary Logo"/>
+        <nav className= "sticky flex top-0 justify-between w-screen items-center px-4 bg-gradient-to-r from-emerald-800 to-[310px] to-emerald-400 text-white ">
+            <Link className="p-1 mr-40 flex-shrink-0" href="/">
+                <Image width={100} height={100} src={logo} alt="Temporary Logo"/>
             </Link>
-            <ul id="navigation" className="flex justify-end items-center max-sm:absolute max-sm:inset">
+            <ul id="navigation" className="flex flex-grow justify-between items-center text-l font-bold max-sm:absolute max-sm:inset">
                 <NavLink href="/art" inner="Art"/>
                 <NavLink href="/artists/manage" inner="Artists"/>
                 <NavLink href="/exhibitions" inner="Exhibitions"/>
                 <NavLink href="/literature" inner="Literature"/>
                 <NavLink href="/media" inner="Media"/>
                 <NavLink href="/sales" inner="Sales"/>
-                <NavLink href="/login" inner={<Image width={30} height={30} src={userico} alt="User Icon"/>}/>
             </ul>
+            <Link className="p-1 ml-40 flex-shrink-0" href="/">
+                <Image width={30} height={30} src={userico} alt="User Icon"/>
+            </Link>
             {/*<button id="nav-toggle" aria-controls="navigation" aria-expanded="false" className='!shadow-none'>
                 <span className="sr-only">Menu</span>
             </button>*/}
