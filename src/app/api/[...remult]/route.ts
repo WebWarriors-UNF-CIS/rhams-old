df01e8a5cd8600ea124178c4d50e5221fa21c8a0
Harrison <hmichas@gmail.com>
2024-01-29 15:55:54 -0500

    vercel and remult connection?

--- src/app/api/[...remult]/route.ts
+++ src/app/api/[...remult]/route.ts
@@ -1,11 +1,20 @@
 import { remultNextApp } from 'remult/remult-next'
+import { createPostgresDataProvider } from "remult/postgres"
 import { ArtPiece } from '../../shared/art'
 import { User } from '../../shared/user'
 import { Artist } from '../../shared/artist'
 import { Exhibit } from '../../shared/exhibit'
 
-export const api = remultNextApp({
-    entities: [ArtPiece, User, Artist, Exhibit]
+const dataProvider = createPostgresDataProvider({
+    connectionString: process.env["POSTGRES_URL"] || process.env["DATABASE_URL"],
+    configuration: {
+      ssl: Boolean(process.env["POSTGRES_URL"]),
+    },
+  })
+
+const api = remultNextApp({
+    entities: [ArtPiece, User, Artist, Exhibit],
+    dataProvider: createPostgresDataProvider()
 })
 
 export const { GET, POST, PUT, DELETE } = api
