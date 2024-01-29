8a243a049e19013063e3af5f473ee8b6eb022b92
Harrison <hmichas@gmail.com>
2024-01-29 15:34:31 -0500

    Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import Nav from './components/navbar'
 import { SpeedInsights } from '@vercel/speed-insights/next';
+import { Analytics } from '@vercel/analytics/react';
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Nav></Nav>
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   )
