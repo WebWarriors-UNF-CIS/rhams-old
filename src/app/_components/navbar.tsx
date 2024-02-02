853122cc232252cb3b5b2b0c2da961517afffc84
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2024-02-02 16:27:13 -0500

    made nav responsive

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -1,23 +1,33 @@
+"use client"
 import Link from 'next/link';
 import Image from 'next/image'
 import logo from '/public/images/temp-logo.png'
-import userico from '/public/images/user-icon-lg-w.png'
-
-function NavLink({ href, inner }: { href: string, inner: string | JSX.Element}) { return (
-    <li>
-        <Link href={href} className='px-5'>
-            <span>{inner}</span>
-        </Link>
-    </li>
-)}
+import userico from '/public/images/user-icon-lg-b.png'
+import { useState } from 'react';
 
 export default function Nav() {
+    const [isExpanded, setExpanded] = useState(false);
+
+    function NavLink({ href, inner }: { href: string, inner: string }) { return (
+        <li className='py-4 pt-[17px] hover:bg-emerald-500'>
+            <Link href={href} className='px-3'>
+                <span onClick={toggleNav}>{inner}</span>
+            </Link>
+        </li>
+    )}
+
+    const toggleNav = () => {
+        var nav = document.getElementById('navigation');
+        nav!.classList.toggle('max-md:block');
+        setExpanded(!isExpanded);
+    };
+
     return (
-        <nav className= "sticky flex top-0 justify-between w-screen items-center px-4 bg-gradient-to-r from-emerald-800 to-[310px] to-emerald-400 text-black">
-            <Link className="p-1 mr-40 flex-shrink-0" href="/">
+        <nav className= "sticky flex top-0 justify-end md:justify-between w-screen items-center px-4 bg-gradient-to-r from-emerald-800 to-[310px] to-emerald-400 text-black">
+            <Link className="p-1 mr-32 grow shrink-0 justify-self-start" href="/">
                 <Image width={100} height={100} src={logo} alt="Temporary Logo"/>
             </Link>
-            <ul id="navigation" className="flex flex-grow justify-between items-center text-l max-sm:absolute max-sm:inset max-sm:hidden">
+            <ul id="navigation" className={`md:flex flex-grow justify-between items-center text-l max-md:bg-emerald-400/75 max-md:top-0 max-md:pt-[58px] max-md:h-screen max-md:absolute max-md:inset max-md:right-0 ${isExpanded ? '' : 'max-md:hidden'}`}>
                 <NavLink href="/art" inner="Art"/>
                 <NavLink href="/artists" inner="Artists"/>
                 <NavLink href="/exhibitions" inner="Exhibitions"/>
@@ -25,12 +35,13 @@ export default function Nav() {
                 <NavLink href="/media" inner="Media"/>
                 <NavLink href="/sales" inner="Sales"/>
             </ul>
-            <Link className="p-1 ml-40 flex-shrink-0" href="/">
+            <Link className="z-10 p-1 ml-[6%] max-md:mr-3 shrink-0" href="/">
                 <Image width={30} height={30} src={userico} alt="User Icon"/>
             </Link>
-            {<button id="nav-toggle" aria-controls="navigation" aria-expanded="false" className='!shadow-none'>
+            <button id="nav-toggle" aria-controls="navigation" aria-expanded={isExpanded} onClick={toggleNav} className='z-10 md:hidden !shadow-none border-y-[0.15rem] rounded-sm w-6 h-4 p-0 border-black'>
                 <span className="sr-only">Menu</span>
-            </button>}
+                <span className="block h-0.5 w-6 rounded-2xl bg-black"></span>
+            </button>
         </nav>
     );
 } 
