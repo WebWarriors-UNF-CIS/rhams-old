c1960f3d446ec16b94f3c486f15bf5eebbe529f0
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2024-02-21 18:21:17 -0500

    whitespace and anchor tags changed to Links

--- src/app/_components/card-art.tsx
+++ src/app/_components/card-art.tsx
@@ -13,14 +13,14 @@ export default function ArtCard({ art, showModal }: { art: ArtPiece, showModal: 
   
   return (
     <div className='card min-h-[168px] grid gap-x-1 grid-cols-[auto_100px] grid-rows-[1rem_1rem_1.4rem_auto]' key={art.id} onClick={() => showModal(art.id)}>
-        <h3 className='whitespace-pre'>{art.title}</h3>
-        <div className="group relative row-span-3">
-          <Image src={art.imageUrl} width={100} height={100} alt={art.description} className='relative peer border border-black' priority={true} />
-          <p className='hidden group-hover:block absolute top-0 bg-slate-900/80 text-white p-1'>{art.description}</p>
-        </div>
-        <p>{artist?.firstName}</p>
-        <p>{art.medium}</p>
-        <p>{art.location}</p>
+      <h3 className='whitespace-pre'>{art.title}</h3>
+      <div className="group relative row-span-3">
+        <Image src={art.imageUrl} width={100} height={100} alt={art.description} className='relative peer border border-black' priority={true} />
+        <p className='hidden group-hover:block absolute top-0 bg-slate-900/80 text-white p-1'>{art.description}</p>
       </div>
+      <p>{artist?.firstName}</p>
+      <p>{art.medium}</p>
+      <p>{art.location}</p>
+    </div>
   )
 }
--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -6,6 +6,7 @@ import { Artist } from '../_shared/artist';
 import Head from 'next/head';
 import ArtistCard from '../_components/card-artist';
 import '../globals.css'
+import Link from 'next/link';
 
 const artistRepo = remult.repo(Artist);
 
@@ -23,48 +24,40 @@ export default function ArtistPage() {
       
       <div>
         <h1 className="text-center justify-text-3xl font-bold p-12 dark:text-white">Artists</h1>
-      <div className="grid grid-cols-7">
-        <div className='col-span-1'>
-          <h1 className="ml-10 dark:text-white text-xl  font-semibold whitespace-nowrap pr-1 pt-2">Find an Artist</h1>
-        </div>
-        <div className="mt-3">
-          <input
-            type="text"
-            placeholder="First Name"
-            className="px-1  border rounded-lg focus:outline-none focus:ring focus:border-emerald-500"
-          />
+        <div className="grid grid-cols-7">
+          <div className='col-span-1'>
+            <h1 className="ml-10 dark:text-white text-xl  font-semibold whitespace-nowrap pr-1 pt-2">Find an Artist</h1>
           </div>
-        <div className="mt-3">
-          <input
-            type="text"
-            placeholder="Last Name"
-            className="px-1 ml-3 border rounded-lg focus:outline-none focus:ring focus:border-emerald-500"
-          />
-        </div>
-        <div className="mt-3">
-          <button className="ml-3 bg-emerald-500 text-white rounded-md p-1 hover:bg-emerald-600 hover:shadow">
-            Search
-          </button>
-        </div>
-        <div className="col-span-3">
-          <a href="../artists/create"><button className="ml-10 bg-emerald-500 text-white rounded-md  mt-3 p-1 hover:bg-emerald-600 hover:shadow">
-              Add Artist
-          </button></a>
+          <div className="mt-3">
+            <input
+              type="text"
+              placeholder="First Name"
+              className="px-1  border rounded-lg focus:outline-none focus:ring focus:border-emerald-500"
+            />
+          </div>
+          <div className="mt-3">
+            <input
+              type="text"
+              placeholder="Last Name"
+              className="px-1 ml-3 border rounded-lg focus:outline-none focus:ring focus:border-emerald-500"
+            />
+          </div>
+          <div className="mt-3">
+            <button className="ml-3 btn-green"> Search </button>
+          </div>
+          <div className="col-span-3">
+            <Link href="../artists/create"><button className="ml-10 mt-3 btn-green"> Add Artist </button></Link>
+          </div>
         </div>
       </div>
-      </div>
-
-
-      
 
       <div className='flex flex-col justify-around items-center mx-auto mt-10'>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-8 mx-auto">
-            {artists.map((artist) => (
+          {artists.map((artist) => (
             <ArtistCard key={artist.id} artist={artist} canEditAndDelete={false} revalidate={1} UIRefresh={function (): void {} } />
-            ))}
+          ))}
         </div>
       </div>
-      </div>
-    
+    </div>
   );
 }
