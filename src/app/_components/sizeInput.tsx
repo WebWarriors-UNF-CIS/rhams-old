6ef86a82bad2cfa3e9e331e28ee73ac5df152b7e
Brendon Jensen <94388382+BigFoot-adventures@users.noreply.github.com>
2024-02-28 16:24:05 -0500

    added min value to size input

--- src/app/_components/sizeInput.tsx
+++ src/app/_components/sizeInput.tsx
@@ -1,15 +1,17 @@
 export default function SizeInput({ id }: { id: string}) {
   return (
-  <div className='input grow dark:text-white'>
-    <label htmlFor={id.toLowerCase()}>{id}:</label>
+  <div className='input !grow-0 dark:text-white'>
+    <label htmlFor={id.toLowerCase()}>{id}</label>
     <div className='bg-white dark:bg-gray-700 border rounded border-emerald-950 dark:border-gray-600 flex focus:ring-black focus:border-emerald-500'>
       <input
         type="number"
         id={id.toLowerCase()}
         placeholder='20'
-        className="!inline !w-14 grow !border-none !outline-none appearance-none"
+        min="0"
+        className="!inline !w-16 shrink !border-none !outline-none appearance-none"
       />
-      <div className="dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+      <div>
+        <label htmlFor={`${id.toLowerCase()}Units`} className="!hidden">Units</label>
         <select name="units" id={`${id.toLowerCase()}Units`} className="dark:bg-gray-700 h-full rounded focus:outline-none">
           <option value="in">in</option>
           <option value="ft">ft</option>
