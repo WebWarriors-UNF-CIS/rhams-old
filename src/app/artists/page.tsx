a14b461b7bfe8b2d239321f7891cd12e6c0401be
amberredmond <n01456296@unf.edu>
2024-03-12 13:38:34 -0400

    moved add artist button

--- src/app/artists/page.tsx
+++ src/app/artists/page.tsx
@@ -15,41 +15,40 @@ export default function ArtistPage() {
   useEffect(() => { artistRepo.find({ where: { name: { $contains:filteredArtists}}}).then(setArtists) }, [filteredArtists]);
 
   return (
-    <main className="container mx-auto">
+    <main>
       <Head>
         <title>View Artists</title>
       </Head>
-      <Link href="../artists/create"><button className="btn-green right-4 top-20 absolute"> Add Artist </button></Link>
-      {/* Add a container div with padding or margin */}
-      <div className="pl-20"> {/* or use ml-20 for margin-left */}
-        <h1 className="text-center justify-text-3xl font-medium p-4 m-10 dark:text-white">Artists</h1>
-        <div className="">
-          <div className="grid grid-cols-3 relative">
-            <div className='col-span-2 input sm:space-x-4'>
-              <label className="dark:text-white !text-xl !inline !text-start">Search by name:</label>
-              <input
-                type="text"
-                placeholder="First Name"
-                onChange={(e) => setFilteredArtists(e.target.value)}
-                className="!w-fit sm:!inline "
-              />
-            </div>
-          </div>
-        </div>
+      <h1 className="text-center justify-text-3xl font-medium p-4 m-10 dark:text-white">Artists</h1>
+      
 
-        <div className="w-full overflow-hidden shadow-md mb-4">
-          <div className="bg-gray-100 p-4">
-            <div className="flex justify-between items-center">
-              <div className="text-lg font-medium">Artist Name</div>
-            </div>
+      <div className="p-4">
+        <div className="grid grid-cols-3 relative">
+          <div className='col-span-2 input sm:space-x-4'>
+            <label className="dark:text-white !text-xl !inline !text-start">Search by name:</label>
+            <input
+              type="text"
+              placeholder="First Name"
+              onChange={(e) => setFilteredArtists(e.target.value)}
+              className="!w-fit sm:!inline "
+            />
+            <span><Link href="../artists/create"><button className="btn-green right-4 absolute"> Add Artist </button></Link></span>
           </div>
-          {artists.map((artist) => (
-            <div key={artist.id} className="flex justify-between items-center p-4 border-t border-gray-200">
-              <Link href={`/artists/${artist.id}`}><div className="text-lg">{artist.name}</div></Link>
-            </div>
-          ))}
-        </div>
+          </div>
       </div>
+
+      <div className="w-full overflow-hidden shadow-md mb-4">
+  <div className="bg-gray-100 p-4">
+    <div className="flex justify-between items-center">
+      <div className="text-lg font-medium">Artist Name</div>
+    </div>
+  </div>
+  {artists.map((artist) => (
+    <div key={artist.id} className="flex justify-between items-center p-4 border-t border-gray-200">
+      <Link href={`/artists/${artist.id}`}><div className="text-lg">{artist.name}</div></Link>
+    </div>
+  ))}
+</div>
     </main>
   );
 }
